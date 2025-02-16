d60082d71ce2fbc5b86f5b002dfb9755d91b2caf
sanika1702 <sanikashinde0506@gmail.com>
2025-02-16 15:22:43 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+
+import ProductList from './api/productList';
+import './App.css';
+import Counter from './counter/counter';
+import Todo from './todo/todo';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Counter/>
+      <hr/>
+      <Productlist/> */}
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
